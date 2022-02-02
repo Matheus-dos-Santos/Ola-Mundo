619ba0212dea7f45b59e0d1222250bd22cda7720
Matheus <matheusd760@gmail.com>
2022-02-02 20:22:48 -0300

    listas, tuplas e dicionarios

--- Python/Teste1/main.py
+++ Python/Teste1/main.py
@@ -1,20 +1,53 @@
-from biblioteca import *
+from biblioteca import a,b,c
+
 # Teste 1
+
+print("Hello World!")
+print("Olá Mundo!")
 print(a)# Print
 print(a+1)
 a = 1
 print("Resultado:",a)
 print(a==1)
 print(a==0)
+
 # IF's
+
 if a == 1:# IF
     print("Deus Lo Vult")
 else:#Else
     print("ERROU")
+
 # For
+
 for n in range (5):#For
     print(n)
 print("O Número é:",n)
 print("-----------------")
 print("|",a,"|",b,"|",c,"|",n,"|")
 print("-----------------")
+
+# Listas []
+
+lista_alimentos = ["Arroz", "Feijão", "Ovos"]
+print(lista_alimentos)
+
+lista_alimentos.append("farofa") # Adicionando um novo item a lista
+print(lista_alimentos)
+
+lista_alimentos.pop(1) # Removendo o item 2 da lista
+print(lista_alimentos)
+
+lista_alimentos.remove("Ovos") # Remove um item especifico da lista
+print(lista_alimentos)
+
+# Dicionarios {}
+
+pessoas = {"Matheus": (19, "solteiro", "brasileiro"), "Rodrigo": (19, "solteiro", "brasileiro")}
+print(pessoas)
+
+print(pessoas["Matheus"]) # Printando informações especificas 
+print(pessoas["Rodrigo"])
+
+pessoas["Beatriz"] = 13, "solteira", "brasileira" # Adicionando um novo Dicionario com tuplas ()
+print(pessoas)
