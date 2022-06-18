84d97f71c46579f56d4b6f048d021373358ed1c3
Matheus <matheusd760@gmail.com>
2022-06-18 11:38:54 -0300

    Create Contrato.py

---
+++ Python/Contrato.py
@@ -0,0 +1,26 @@
+# Contrato - Linkedin (SOBRE)
+contrato  =  input ( "Nome: Matheus \n "
+ "Religião: Cristão \n "
+ "Idade: 20 anos \n "
+ "Localidade: Diadema- São Paulo \n "
+ "Profissão: Estudante \n "
+ "------------------------------------------------- --------------------------------------------------------- \n "
+ "| Estudante da computação, estou em busca de estágio e mais experiência no mercado de trabalho,            | \n "
+ "| gosto de aprender coisas novas e aprimorar minhas habilidades.                                           | \n "
+ "| gosto de: TI, programação, programação e jogos. Atualmente estudando mais Back-End: Python               | \n "
+ "------------------------------------------------- --------------------------------------------------------- \n "
+ "Contratar? [Sim|Não]" ). superior ()
+
+se  contrato  ==  "SIM" :
+ print ( "Meus Parabéns pelo novo funcionário incrível!!!" )
+
+mais :
+ print ( "Usuários... \n "
+ "Esbarrou o dedo no botão errado? tente de novo..." )
+ contrato  =  input ( "Contratar? [Sim|Não]" ). superior ()
+ se  contrato  ==  "SIM" :
+  print ( "Que bom que acertou desta vez! \n "
+        "Meus Parabéns pelo novo funcionário incrível!!!" )
+ mais :
+  print ( "Ok. \n "
+  "Deus Abençoe!" )
