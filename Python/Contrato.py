3a7dee1e0ec6d5aa006b548b0d032675f5996a35
Matheus <matheusd760@gmail.com>
2022-07-20 13:21:55 -0300

    Update Contrato.py

--- Python/Contrato.py
+++ Python/Contrato.py
@@ -1,26 +1,27 @@
 # Contrato - Linkedin (SOBRE)
-contrato  =  input ( "Nome: Matheus \n "
- "Religião: Cristão \n "
- "Idade: 20 anos \n "
- "Localidade: Diadema- São Paulo \n "
- "Profissão: Estudante \n "
- "------------------------------------------------- --------------------------------------------------------- \n "
- "| Estudante da computação, estou em busca de estágio e mais experiência no mercado de trabalho,            | \n "
- "| gosto de aprender coisas novas e aprimorar minhas habilidades.                                           | \n "
- "| gosto de: TI, programação, programação e jogos. Atualmente estudando mais Back-End: Python               | \n "
- "------------------------------------------------- --------------------------------------------------------- \n "
- "Contratar? [Sim|Não]" ). superior ()
+contrato = input("""Nome: Matheus dos Santos Ferreira
+Religião: Cristão
+Idade: 20 anos 
+Localidade: Diadema- São Paulo 
+Profissão: Estudante
+Linguagem preferida: Python
+------------------------------------------------- ---------------------------------------------------------
+| Estudante da computação, estou em busca de estágio e mais experiência no mercado de trabalho,           |
+| gosto de aprender coisas novas e aprimorar minhas habilidades.                                          |
+| gosto de: TI, programação, programação e jogos. Atualmente estudando mais Back-End: Python              |
+------------------------------------------------- ---------------------------------------------------------
+Contratar? [Sim|Não]""").upper()
 
-se  contrato  ==  "SIM" :
- print ( "Meus Parabéns pelo novo funcionário incrível!!!" )
+if contrato == "SIM":
+    print("Meus Parabéns pelo novo funcionário incrível!!!")
 
-mais :
- print ( "Usuários... \n "
- "Esbarrou o dedo no botão errado? tente de novo..." )
- contrato  =  input ( "Contratar? [Sim|Não]" ). superior ()
- se  contrato  ==  "SIM" :
-  print ( "Que bom que acertou desta vez! \n "
-        "Meus Parabéns pelo novo funcionário incrível!!!" )
- mais :
-  print ( "Ok. \n "
-  "Deus Abençoe!" )
+else:
+    print("Usuários... \n"
+        "Esbarrou o dedo no botão errado? tente de novo...")
+    contrato  =  input ( "Contratar? [Sim|Não]" ).upper()
+    if  contrato  ==  "SIM":
+        print ( "Que bom que acertou desta vez! \n "
+            "Meus Parabéns pelo novo funcionário incrível!!!" )
+    else:
+        print ( "Ok. \n "
+            "Deus Abençoe!")
